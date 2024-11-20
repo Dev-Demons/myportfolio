099665704a62e6ba7adc52376973738e18099302
Dev-Demons <bnicola1023@gmail.com>
2024-11-20 20:51:53 +0000

    chore: stylesheet

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div variants={slideIn('left', "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.heroSubText}>Get in Touch</p>
-        <h3 className={styles.heroHeadText}>Contact.</h3>
+        <h3 className={styles.heroHeadText}>Contact</h3>
         <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Name.</span>
