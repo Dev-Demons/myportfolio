920fa0bc0b670ce497ad1f155e3c7684fa16dd6a
Dev-Demons <bnicola1023@gmail.com>
2025-01-14 12:52:35 +0000

    chore: vite config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  server: {
+    host: '0.0.0.0', // Expose server to all interfaces
+    port: 5173, // Or use a different port
+  },
 })
